b87d8dd6d2edbb65234db06fe7980a84dc7cbe45
janaAlmomani <jana2000510@gmail.com>
2023-04-29 07:51:03 +0000

    CC5 done

--- tests/test_ll.py
+++ tests/test_ll.py
@@ -1,33 +1,42 @@
 import pytest
 from LinkedList.LinkedList import *
 
-# Test empty linked list
-def test_empty_ll():
-    ll = LinkedList()
-    expected = "Empty LinkedList"
-    actual = str(ll)
-    assert expected == actual
-
-# Test insert 6 nodes
-def test_insert(ll):
-    expected = "{ 9 } -> { 6 } -> { 1 } -> { 2 } -> { 5 } -> { 3 } -> NULL"
-    actual = str(ll)
-    assert expected == actual
-
-# Test includes 
-def test_includes(ll):
-    expected = "True"
-    actual = str(ll.includes(9))
-    assert expected == actual
-
 @pytest.fixture
 def ll():
     ll = LinkedList()
-    ll.insert(3)
-    ll.insert(5)
-    ll.insert(2)
     ll.insert(1)
-    ll.insert(6)
-    ll.insert(9)
-    ll.includes(9)
+    ll.insert(2)
+    ll.insert(3)
     return ll
+
+# Test empty linked list
+def test_empty_linked_list():
+    LL = LinkedList()
+    expected = "Empty LinkedList"
+    actual = str(LL)
+    assert expected == actual
+
+# Test insert 3 nodes
+def test_insert(ll):
+
+    expected = "{ 3 } -> { 2 } -> { 1 } -> NULL"
+    actual = str(ll)
+    assert expected == actual
+    
+# Test linked list to string
+def test_to_string(ll):
+    assert ll.to_string() == "{ 3 } -> { 2 } -> { 1 } -> NULL"
+
+# Test includes 
+def test_includes(ll):
+    expected = "False"
+    actual = str(ll.includes(9))
+    assert expected == actual
+
+# Test includes 
+def test_includes_two(ll):
+    assert ll.includes(1) == True
+    assert ll.includes(2) == True
+    assert ll.includes(3) == True
+    assert ll.includes(4) == False
+
